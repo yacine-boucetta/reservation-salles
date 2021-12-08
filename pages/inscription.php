8bbd5b9bb078b52a24a255e90d3d3500f0197b61
thomas-dellacase <thomas.dellacase@laplateforme.io>
2021-12-08 09:53:33 +0100

    Merge branch 'classUser'

---
+++ pages/inscription.php
@@ -0,0 +1,23 @@
+<?php
+
+require '../config/classUser.php';
+
+if(isset($_POST['sign_up'])){
+$inscription= new User();
+$inscription->user_inscription($_POST['login'],$_POST['password'],$_POST['password2']);
+}
+?>
+
+
+
+
+<main>
+<h2>Sign up</h2>
+    <form  method='post'>
+        <input type="text" name="login" required="" placeholder="Username">
+        <input type="password" name="password" required="" placeholder="password">
+        <input type="password" name="password2" required="" placeholder="veuillez confirmer votre mot de passe">
+        <button type="submit" name="sign_up" value="sign_up">Valider</button>
+    </form>
+</main>
+
