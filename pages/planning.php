b5e5b4665574a79e84e568f9b50a45c7529e11f1
thomas-dellacase <thomas.dellacase@laplateforme.io>
2021-12-08 11:21:18 +0100

    debut du tableau

--- pages/planning.php
+++ pages/planning.php
@@ -1,10 +1,29 @@
 <?php
+require '../config/db.php'
 
 ?>
 <?php
 require '../template/header.php';
 ?>
+<article>
+    <?php
+    
 
+    echo "<p><center><h4>Planning</h4></center></p>";
+    echo "<table>
+    <tr>
+    <th>Lundi</th>
+    <th>Mardi</th>
+    <th>Mercredi</th>
+    <th>Jeudi</th>
+    <th>Vendredi</th>
+    <th>Samedi</th>
+    <th>Dimanche</th>
+    </tr>";
+
+
+    ?>
+</article>
 
 
 <?php
