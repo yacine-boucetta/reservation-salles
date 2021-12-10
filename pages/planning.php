d34064e4c0ea30598b93faa1af94ed5a86cac5c2
yacine-boucetta <yacine.boucetta@laplateforme.io>
2021-12-10 10:37:05 +0100

    debut planning

--- pages/planning.php
+++ pages/planning.php
@@ -4,22 +4,33 @@ require '../config/db.php'
 ?>
 <?php
 require '../template/header.php';
+$jour_semaine=date('d/m', strtotime(''));
 ?>
 <article>
-    <?php
-    
+<h1>Planning <?php echo $jour_semaine = date('Y', time());?></h1>
+        <h2>Semaine <?php echo $jour_semaine = date('W', time());?></h2>
+        <table>
+            <thead>
+                <tr>
+                    <th class="vide"></th>
+                    <th class="jour">Lundi <?php echo $jour_semaine = date('d/m', strtotime('monday this week'));?></th>
+                    <th class="jour">Mardi <?php echo $jour_semaine = date('d/m', strtotime('tuesday this week'));?></th>
+                    <th class="jour">Mercredi <?php echo $jour_semaine = date('d/m', strtotime('wednesday this week'));?></th>
+                    <th class="jour">Jeudi <?php echo $jour_semaine = date('d/m', strtotime('thursday this week'));?></th>
+                    <th class="jour">Vendredi <?php echo $jour_semaine = date('d/m', strtotime('friday this week'));?></th>
+                </tr>                              
+            </thead>
+            <tbody>
+                <?php
+                for($i=1;$i<=5;$i++){
+                   
+                    for($j=1;$j<=11;$j++){
 
-    echo "<p><center><h4>Planning</h4></center></p>";
-    echo "<table>
-    <tr>
-    <th>Lundi</th>
-    <th>Mardi</th>
-    <th>Mercredi</th>
-    <th>Jeudi</th>
-    <th>Vendredi</th>
-    </tr>";
-    
-    ?>
+                    }
+                }        
+                ?>
+            </tbody>
+        </table>
 </article>
 
 
